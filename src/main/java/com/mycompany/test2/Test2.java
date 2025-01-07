87fbe5049116427d2859bef5abdb531927cc15ad
curti <curti@192.168.1.212>
2025-01-07 17:02:47 -0500

    Linked to account Signed-off-by:curti <curti@192.168.1.212>

--- src/main/java/com/mycompany/test2/Test2.java
+++ src/main/java/com/mycompany/test2/Test2.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.test2;
 
-/**
- *
- * @author curti
- */
 public class Test2 {
 
     public static void main(String[] args) {
