3985de05e5393ea1704a93bb7e9c2488ead4d829
curti <curti@192.168.1.212>
2025-01-07 17:24:12 -0500

    Did the first video, Made Main,m GamePanel, and implemented Runnable Signed-off-by:curti <curti@192.168.1.212>

---
+++ src/main/java/com/mycompany/test2/GamePanel.java
@@ -0,0 +1,25 @@
+package com.mycompany.test2;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable{
+    
+    final int originalTileSize = 16; //Might not need? not sure.
+    
+    int screenWidth = 1300;
+    int screenHeight = 768;
+    
+    Thread gameThread;
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.white);
+        this.setDoubleBuffered(true);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
--- src/main/java/com/mycompany/test2/Test2.java
+++
@@ -1,9 +0,0 @@
-
-package com.mycompany.test2;
-
-public class Test2 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
